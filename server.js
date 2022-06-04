425fac9430ed1b80b575a2438e121cc504e82b12
Tim Blais <tblais189@gmail.com>
2022-06-04 14:23:24 -0400

    new additions plus gitignore

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const res = require('express/lib/response');
+const bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function(){
     console.log('listening on 3000')
@@ -7,4 +11,8 @@ app.listen(3000, function(){
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/items', (req,res) => {
+    console.log('hello yall')
 })
