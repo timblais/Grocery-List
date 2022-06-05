1c8a27e3b83332bbd7f195b3933f53fd66b2c8bc
Tim Blais <tblais189@gmail.com>
2022-06-05 17:19:19 -0400

    changes to server.js and added some new modules

--- server.js
+++ server.js
@@ -5,20 +5,36 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 
-MongoClient.connect('mongodb-connection-string', (err, client) => {
-    // ... do something here
+MongoClient.connect(`mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@cluster0.8baa0j1.mongodb.net/?retryWrites=true&w=majority`, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('items')
+    const itemCollection = db.collection('items')
+
+    app.set('view engine', 'ejs')
+    
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.listen(3000, function(){
+        console.log('listening on 3000')
+    })
+
+    app.get('/', (req,res) => {
+        db.collection('items').find().toArray()
+        .then(results => {
+          console.log(results)
+        })
+        .catch(error => console.error(error))
+        res.render('index.ejs', { items: results })
+    })
+
+    app.post('/items', (req,res) => {
+        itemCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+
   })
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.listen(3000, function(){
-    console.log('listening on 3000')
-})
-
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/items', (req,res) => {
-    console.log(req.body)
-})
